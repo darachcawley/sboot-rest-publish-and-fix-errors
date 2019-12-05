9623647b88f8f72b70be6440ff37b6622104d791
darachcawley <darachcawley@gmail.com>
2019-12-05 17:09:30 +0000

    dead letter pattern to insert errors into topic, persist to DB, check for them to be fixed and resend them to original queue to be reprocessed

--- src/main/java/com/redhat/training/gpte/springboot/Application.java
+++ src/main/java/com/redhat/training/gpte/springboot/Application.java
@@ -17,8 +17,15 @@
 package com.redhat.training.gpte.springboot;
 
 import org.apache.activemq.jms.pool.PooledConnectionFactory;
+import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.amqp.AMQPComponent;
 import org.apache.camel.component.servlet.CamelHttpTransportServlet;
+import org.apache.camel.dataformat.bindy.csv.BindyCsvDataFormat;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.qpid.jms.JmsConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -29,7 +36,7 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 // load regular Spring XML file from the classpath that contains the Camel XML DSL
 @ImportResource({"classpath:spring/camel-context.xml"})
-public class Application {
+public class Application extends RouteBuilder{
 
     /**
      * A main method to start this application.
@@ -37,6 +44,74 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
+    @Override
+    public void configure () throws Exception {
+    	
+	  onException(IllegalArgumentException.class)
+	  	.log(LoggingLevel.INFO, "org.fuse.usecase", "onException triggered")
+	    .to("direct:error")
+	    .handled(true)
+	    ;
+	
+	  BindyCsvDataFormat format = new BindyCsvDataFormat(org.acme.Customer.class);
+	  format.setLocale("default");
+	  
+	  restConfiguration().component("servlet").port("8080")
+	  	.host("localhost");
+
+	  rest("/service")
+	  	.post("/customers")
+//		  	.to("amqp:queue:inputQueue")
+	  	;
+
+	  // ERROR LOGGER
+	  from("direct:error")
+	  	.log(LoggingLevel.INFO, "org.fuse.usecase", "processing error...")
+	  	.process(new Processor() {
+			
+			@Override
+			public void process(Exchange exchange) throws Exception {
+				final Throwable ex = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Throwable.class);
+				exchange.getIn().setHeader("error-code", "111");
+				exchange.getIn().setHeader("error-message", ex.getMessage());
+			}
+		})
+	  	.to("amqp:topic:errorNotification")
+	    ;
+	  
+	  // ERROR HANDLER
+	  from("amqp:topic:errorNotification")
+	  	  .setExchangePattern(ExchangePattern.InOnly)
+	  	  .log(LoggingLevel.INFO, "org.fuse.usecase", "error triggered from topic")
+	      .to("sql:insert into USECASE.T_ERROR (ERROR_CODE, ERROR_MESSAGE, MESSAGE, STATUS) VALUES (:#${headers.error-code}, :#${headers.error-message}, :#${body}, 'ERROR')")
+	  	;
+	  
+	  // CORRECT ERRORS
+	  from("sql:select MESSAGE, ID from USECASE.T_ERROR where STATUS = 'FIXED' ?consumer.onConsume=update USECASE.T_ERROR set STATUS='CLOSE' where ID = :#ID")
+  		  .split(body())
+	  	  .log(LoggingLevel.INFO, "org.fuse.usecase", "error fixed")
+	  	  .setBody().simple("${body[message]}")
+	  	  .to("amqp:queue:inputQueue")
+	  	;
+	  
+	  // SPLIT and TRANSFORM
+//	  from("amqp:queue:inputQueue")
+	  from("direct:input")
+	  	  .setExchangePattern(ExchangePattern.InOnly)
+	      .split()
+	      .tokenize(";")
+	      .log(LoggingLevel.INFO, "org.fuse.usecase", "msg tokenized")
+	      .unmarshal(format)
+	      .log(LoggingLevel.INFO, "org.fuse.usecase", "msg unmarshalled")
+	      .to("dozer:customerToAccount?mappingFile=transformation.xml&sourceModel=org.acme.Customer&targetModel=org.globex.Account")
+	      .log(LoggingLevel.INFO, "org.fuse.usecase", "msg ran through dozer")
+	      .marshal().json(JsonLibrary.Jackson)
+	      .to("amqp:queue:accountQueue")
+	      ;
+	  
+    }
+    
     @Bean
     ServletRegistrationBean servletRegistrationBean() {
         ServletRegistrationBean servlet = new ServletRegistrationBean(
